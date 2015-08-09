8e04d3e3d08c8743b186eb51f2236365da05b27b
Prelang Builder <builder@prelang.com>
2015-08-09 19:15:27 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Band' to allow for create, update

--- app/admin/band.rb
+++ app/admin/band.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Band do
+  permit_params :name, :city, :email, :website
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
